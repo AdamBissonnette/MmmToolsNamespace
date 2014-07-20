93554af98bb9be08e93431aa95acdc44af58621f
Adam B <gadimus@gmail.com>
2014-07-20 15:13:25 -0600

    updated string-tools.php logic

--- string-tools.php
+++ string-tools.php
@@ -6,16 +6,45 @@ function delimitList($data, $format = 'comma')
 	switch ($format)
 	{
 		case 'comma':
-			$formattedList = createCommaList($data);
+			$formattedList = createStringList($data, ", ");
 		break;
 		case 'comma-and':
-			$formattedList = createCommaAndList($data);
+			$formattedList = createStringList($data, ", ", " and ");
 		break;
 	}
 
 	return $formattedList;
 }
 
+function createStringList($data, $delimiter, $lastTermException=NULL)
+{
+	if (!isset($lastTermException))
+	{
+		$lastTermException = $delimiter;
+	}
+
+	$output = '';
+
+	for ($i = 0; $i < count($data); $i++ )
+	{
+		$item = $data[$i];
+
+		if ($i == (count($data) - 2)) //Second Last Term
+		{
+			$output .= $item . $lastTermException;
+		}
+		else if ($i == (count($data) - 1)) //Last Term
+		{
+			$output .= $item;
+		}
+		else
+		{
+			$output .= $item . $delimiter;
+		}
+	}
+
+	return $output;
+}
 
 function createCommaList($data)
 {
