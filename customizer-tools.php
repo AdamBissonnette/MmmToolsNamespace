fcfaf8bb87fe70fb9b26ef5b806e0c3596069175
Adam B <gadimus@gmail.com>
2014-07-29 10:55:27 -0600

    Created lib folder for custom customizer-tools

--- customizer-tools.php
+++ customizer-tools.php
@@ -284,19 +284,6 @@ function admin_scripts() {
 
 }
 
-require_once( ABSPATH . WPINC . '/class-wp-customize-control.php' );
-
-class Mmm_Color_Control extends WP_Customize_Control {
-    public $type = 'text';
-
-    public function render_content() {
-        ob_start(); //Since there isn't a nice way to get this link content we have to use the output buffer
-        $this->link();
-        $link = ob_get_contents();
-        ob_end_clean();
-
-        echo createFormField($this->id, "", $this->value(), "color", array("title" => $this->label, "link" => $link));
-    }
-}
+require_once( 'lib/customizer-classes.php' );
 
 ?>
